2f58f85fe1802259ef14de2e2a8185ee4d704258
Lisa Geist <lbgeist@gmail.com>
2022-06-08 16:55:58 -0400

    begin adding arrows to about and projects

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,6 @@
 // JavaScript Document
 const about = document.getElementById('about');
+const aboutArrow = document.getElementById('about-side-arrow');
 const projects = document.getElementById('projects');
 
 const paragraph = document.createElement('p');
@@ -7,16 +8,24 @@ const paragraph2 = document.createElement('p');
 
 
 about.onclick = function() {
-  about.style.backgroundColor = 'red';
-  let aboutMe = document.getElementsByClassName('about-me');
-  for (let i = 0; i < aboutMe.length; i++) {
-    aboutMe[i].style.display = "inline-block";
-  }
+  if(aboutArrow.style.display === 'none') {
+    aboutArrow.style.display = 'inline-block';
+    let aboutMe = document.getElementsByClassName('about-me');
+    for (let i = 0; i < aboutMe.length; i++) {
+      aboutMe[i].style.display = "none";
+    }
+  } else {
+    aboutArrow.style.display = "none";
+    let aboutMe = document.getElementsByClassName('about-me');
+    for (let i = 0; i < aboutMe.length; i++) {
+      aboutMe[i].style.display = "inline-block";
+    }
+  } 
 }
 
-projects.onclick = function() {
-  projects.style.backgroundColor = 'blue';
-  paragraph2.id = 'projects-p';
-  paragraph2.innerHTML = 'This is another test.';
-  projects.appendChild(paragraph2);
-}
+
+
+
+
+
+
